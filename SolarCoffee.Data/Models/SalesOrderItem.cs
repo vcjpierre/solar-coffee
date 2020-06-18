874271c07c98cc5c01ad3dcef7e3edae5d707357
Jean Pierre Velez <vcjpierre@gmail.com>
2020-06-17 22:26:06 -0400

    Add data models

---
+++ SolarCoffee.Data/Models/SalesOrderItem.cs
@@ -0,0 +1,7 @@
+﻿namespace SolarCoffee.Data.Models {
+    public class SalesOrderItem {
+        public int Id { get; set; } 
+        public int Quantity { get; set; }
+        public Product Product { get; set; }
+    }
+}
