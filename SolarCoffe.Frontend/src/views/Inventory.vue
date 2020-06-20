56781a4f8fa31b9fc6ec3f1ecdeb31077335adcb
Jean Pierre Velez <vcjpierre@gmail.com>
2020-06-20 19:14:09 -0400

    Create the app layout

---
+++ SolarCoffe.Frontend/src/views/Inventory.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="inventory-container">
+    <h1 id="inventoryTitle">
+      Inventory Dashboard
+    </h1>
+    <hr />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-property-decorator";
+
+export default class Inventory extends Vue {}
+</script>
+
+<style scoped></style>
