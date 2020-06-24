7edb7ebdde22f3d6e3ea12753a2f0914afb765cf
Jean Pierre Velez <vcjpierre@gmail.com>
2020-06-24 19:34:43 -0400

    Implementing Typescript Types, Newtonsoft Json.NET Library

--- SolarCoffee.Api/Startup.cs
+++ SolarCoffee.Api/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 using SolarCoffee.Data;
 using SolarCoffee.Services.Customer;
 using SolarCoffee.Services.Inventory;
@@ -32,14 +26,19 @@ namespace SolarCoffee.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
-            services.AddControllers();
+            
+            services.AddControllers().AddNewtonsoftJson(opts => {
+                opts.SerializerSettings.ContractResolver = new DefaultContractResolver {
+                    NamingStrategy = new CamelCaseNamingStrategy()
+                };
+            });
 
-            services.AddDbContext<SolarDbContext>(opts =>
-            {
+            services.AddDbContext<SolarDbContext>(
+                opts => {
                 opts.EnableDetailedErrors();
                 opts.UseNpgsql(Configuration.GetConnectionString("solar.dev"));
             });
-            
+
             services.AddTransient<IProductService, ProductService>();
             services.AddTransient<ICustomerService, CustomerService>();
             services.AddTransient<IInventoryService, InventoryService>();
@@ -49,13 +48,10 @@ namespace SolarCoffee.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
 
             app.UseHttpsRedirection();
-
+            
             app.UseRouting();
             
             app.UseCors(
@@ -67,10 +63,10 @@ namespace SolarCoffee.Api
                     .AllowAnyMethod()
                     .AllowAnyHeader()
                     .AllowCredentials()
-            );
+                );
 
             app.UseAuthorization();
-
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
