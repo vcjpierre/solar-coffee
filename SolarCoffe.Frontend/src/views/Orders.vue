504f3346bd2a31d69d8d15e6347e6c43a5a059c9
Jean Pierre Velez <vcjpierre@gmail.com>
2020-06-27 16:13:29 -0400

    Manage customers and invoices

---
+++ SolarCoffe.Frontend/src/views/Orders.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Hello from Orders view</div>
+</template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+@Component({ name: "Orders" })
+export default class Orders extends Vue {}
+</script>
+
+<style scoped></style>
